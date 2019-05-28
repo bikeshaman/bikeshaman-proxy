44c1f692266a4506a6fb6a23fffbe584e06d181a
Adam Gipril <adam.gipril@icloud.com>
2019-05-28 13:05:45 -0600

    Initial setup completed ---
    - dependencies installed
    - dev scripts written
    - eslint configured
    - webpack configured
    - server setup completed

---
+++ client/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    HI
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('trek-bikes'));
