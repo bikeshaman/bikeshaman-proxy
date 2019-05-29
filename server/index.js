545ded39e53e08df04aa4421e3e834ac1a897e9c
Adam Gipril <adam.gipril@icloud.com>
2019-05-29 09:33:53 -0600

    Rearranged server files

--- server/index.js
+++ server/index.js
@@ -5,5 +5,4 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
-
-module.exports = app;
+app.listen(process.env.PORT || 2000);
--- server/server.js
+++
@@ -1,4 +0,0 @@
-const app = require('./app');
-
-const port = process.env.PORT || 2000;
-app.listen(port);
