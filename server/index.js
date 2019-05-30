06055e2c78600c1d6b7c86fa1f56afd5b71ad702
Adam Gipril <adam.gipril@icloud.com>
2019-05-30 11:32:17 -0600

    Buying zone renders, but needs CORS

--- server/index.js
+++ server/index.js
@@ -4,28 +4,6 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
-app.use('/buying-zone', express.static(path.join(__dirname, '../buying-zone/client/dist')));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.listen(process.env.PORT || 2000);
-
-const { Product } = require('../databases/buying-zone_db');
-const template = require('./template');
-
-/**
- * Gets the product associated with the id passed in the url. Sends the retrieved product back as
- * JSON.
- * @param {number} id - The product's human-readable id (think model number)
- * @returns {Product} - Sends the queried product back in the response
- */
-app.get('/products/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    let product = await Product.findOne({ id });
-    product = JSON.stringify(product);
-    const response = template({ product });
-    res.send(response);
-  } catch (err) {
-    // TODO: improve error handling
-    res.sendStatus(400);
-  }
-});
+app.listen(process.env.PORT || 3000);
